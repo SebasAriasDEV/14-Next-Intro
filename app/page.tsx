b88648be49ad9ba49926623178557aeb87c7aaaa
Sebas Dev <sebasariasdev@gmail.com>
2023-07-04 18:53:13 +0800

    Small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-
+import MainTitle from '@/components/MainTitle';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <h1 className='font-bold text-2xl'>
-        Mi primer proyecto de Next JS (13.2)
-      </h1>
-      <hr className='my-4' />
+      <MainTitle />
       <Link
         href='/primera'
         className='bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded'
---
+++ components/MainTitle.tsx
@@ -0,0 +1,8 @@
+export default function MainTitle () {
+    return (
+        <>
+      <h1 className='font-bold text-2xl'>Next Project 13.0</h1>
+      <hr className='my-4' />
+    </>
+    );
+}
